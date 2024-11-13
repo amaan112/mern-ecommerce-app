f773c77251b102251ebe55e70c2b8f70c9aa25e8
amaan112 <143351918+amaan112@users.noreply.github.com>
2024-11-14 01:13:20 +0530

    Updated

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import cors from 'cors';
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import path from 'path';
+import {fileURLToPath} from 'url';
 // config env
 dotenv.config();
 
@@ -15,6 +16,9 @@ const app = express();
 
 
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors())
 app.use(express.json())
